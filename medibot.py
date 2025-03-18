7d43f548e12eb6d0b640111cfb44404622f2182c
Shilpa Jha <shilpajmd123@gmail.com>
2025-03-18 15:03:51 +0530

    fix error

--- medibot.py
+++ medibot.py
@@ -1,15 +1,14 @@
 import os
 import streamlit as st
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
-from langchain_huggingface import HuggingFaceEmbeddings
 
 
 DB_FAISS_PATH="vectorstore/db_faiss"
-st.cache_resource
+@st.cache_resource
 def get_vectorstore():
     embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     db=FAISS.load_local(DB_FAISS_PATH, embedding_model,allow_dangerous_deserialization=True)
